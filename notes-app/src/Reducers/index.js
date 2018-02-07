be38e8c9196d133b3fb3f01da1b1382312bd72af
unknown <brcampbell001@outlook.com>
2018-02-07 15:58:11 -0600

    Implementing Redux-Forms in theory possibly maybe meh

--- notes-app/src/Reducers/index.js
+++ notes-app/src/Reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import notes from './notes'
 
-const rootReducer = combineReducers({ notes });
+const rootReducer = combineReducers({ notes, form: formReducer });
 
 export default rootReducer;
