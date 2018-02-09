aa023da6fb53d3814e52bdb4b0760f627e72a6d1
unknown <brcampbell001@outlook.com>
2018-02-08 22:30:19 -0600

    Built Add Note Form / Logging in Console

--- note-mate-v2/src/App.js
+++ note-mate-v2/src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 
-import './App.css';
+// import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      note: ''
+    }
+  }
+
+  handleChange(e) {
+    this.setState ({
+      note: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.note);
+  }
+
   render() {
+    let note;
     return (
-          <h1>Ben's KISS attempt at building note app without backend</h1>
+      <div>
+        <h1>Ben's KISS/Non-fluffy Attempt/Version 2.0 Note App</h1>
+        <hr/>
+        {/* <ul>
+              {this.props.notes.map((note, i) => <li key={i}>{note.name}</li>)}
+        */}
+
+      <div>
+        <h3>Add Note Form</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
     );
   }
 }
