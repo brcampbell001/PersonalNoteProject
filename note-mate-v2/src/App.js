00f30c3821b6d6a4e693f0d75e962361e72c5b37
unknown <brcampbell001@outlook.com>
2018-02-08 22:07:16 -0600

    Archived Prev V - V2 React App Created

---
+++ note-mate-v2/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+          <h1>Ben's KISS attempt at building note app without backend</h1>
+    );
+  }
+}
+
+export default App;
--- notes-app/src/Reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-import notes from './notes'
-
-
-const rootReducer = combineReducers({ notes, form: formReducer });
-
-export default rootReducer;
